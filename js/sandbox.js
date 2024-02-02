603be583dcdfa3423fe8e7a1e316ef23cef184ce
jelilking <abduljelilozigi@gmail.com>
2024-02-02 10:30:17 +0100

    side nav rectified

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,13 +1,20 @@
 // typing animation
 // let typed = new Typed(".typing", {
-//   Strings: ["Web Designer", "Web Developer", "Software Engineer"],
+//   Strings: [" ", "Web Designer", "Web Developer", "Software Engineer"],
 //   typeSpeed: 100,
 //   backSpeed: 60,
 //   loop: true,
 // });
 
-// const NavMenuBtn = document.querySelector(".menu-btn");
-// const MenuItems = document.querySelector(".nav-items");
-// NavMenuBtn.addEventListener("click", () => {
-//   MenuItems.classList.toggle("show");
-// });
+const navMenuBtn = document.querySelector(".menu-btn");
+const MenuItems = document.querySelector(".aside");
+navMenuBtn.addEventListener("click", () => {
+  MenuItems.classList.toggle("show");
+});
+
+const navMenu = document.querySelectorAll(".navMenus");
+navMenu.forEach((link) => {
+  link.addEventListener("click", () => {
+    MenuItems.classList.remove("show");
+  });
+});
